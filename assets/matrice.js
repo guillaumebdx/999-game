9401fbb346a809d11537ead434e3c3422962da32
Guillaume HARARI <guillaumeharari@hotmail.com>
2020-12-20 18:20:32 +0100

    Fix js behaviors

--- assets/matrice.js
+++ assets/matrice.js
@@ -10,50 +10,53 @@ for (let i=0; i< blocks.length; i++) {
         let currentNumber = parseInt(event.target.textContent);
         let blockForbidden = true;
         if (previousX === currentX) {
-            console.log('meme ligne')
+//            console.log('meme ligne')
             if (currentY -1 === previousY || currentY +1 === previousY) {
                 blockForbidden = false;
-                console.log('block ligne autorisé')
+               // console.log('block ligne autorisé')
             }
         }
         if (previousY === currentY) {
-            console.log('même collone')
+         //   console.log('même collone')
             if (currentX -1 === previousX || currentX +1 === previousX) {
-                console.log('juste dessus dessous autorisé')
+               // console.log('juste dessus dessous autorisé')
                 blockForbidden = false;
             }
         }
 
         if (previousX !== currentX && previousY !== currentY) {
-            console.log('diag')
+        //    console.log('diag')
             if (previousX -1 === currentX && previousY -1 === currentY || previousY +1 === currentY) {
-                console.log('diag haut autorisé')
+             //   console.log('diag haut autorisé')
                 blockForbidden = false;
             }
 
             if (previousX +1 === currentX && previousY -1 === currentY || previousY +1 === currentY) {
-                console.log('diag bas autorisé')
+             //   console.log('diag bas autorisé')
                 blockForbidden = false;
             }
         }
 
         if (previousX === 0 && previousY === 0) {
             blockForbidden = false;
-            console.log('autorisé')
+       //     console.log('autorisé')
         }
         if (previousNumber !== 0) {
             if (previousNumber !== currentNumber) {
                 blockForbidden = true;
-                console.log('choisir un chiffre identique')
+            //    console.log('choisir un chiffre identique')
             }
         }
-
+        if (event.target.classList.contains('forbiddenLabel')) {
+            blockForbidden = true;
+        }
         if (blockForbidden) {
             event.preventDefault();
         } else {
             previousX = parseInt(blocks[i].getAttribute('data-x'));
             previousY = parseInt(blocks[i].getAttribute('data-y'));
             previousNumber = parseInt(event.target.textContent);
+            event.target.classList.add('forbiddenLabel')
         }
 
 
