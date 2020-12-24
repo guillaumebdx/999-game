68d8b8e271d7fc9715f1ddf86dcf10be1f3c1abf
Guillaume HARARI <guillaumeharari@hotmail.com>
2020-12-24 12:03:37 +0100

    add level crud

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Level;
+use App\Form\LevelType;
 use App\Repository\LevelRepository;
 use App\Repository\MatriceRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,13 +33,55 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/level/{level}", name="level")
+     * @Route("switch/level/{level}", name="level")
      */
-    public function updateLevel(Level $level, EntityManagerInterface $entityManager)
+    public function switchLevel(Level $level, EntityManagerInterface $entityManager)
     {
         $this->getUser()->setLevel($level);
         $entityManager->persist($this->getUser());
         $entityManager->flush();
         return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/create/level", name="create_level")
+     */
+    public function createLevel(Request $request,
+                                EntityManagerInterface $entityManager,
+                                LevelRepository $levelRepository)
+    {
+        $lastLevel = $levelRepository->findOneBy([],['level' => 'DESC']);
+
+        $level = new Level();
+        $level->setLevel($lastLevel->getLevel() +1);
+        $form = $this->createForm(LevelType::class, $level);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($level);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_index');
+        }
+        return $this->render('level/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/update/level/{level}", name="update_level")
+     */
+    public function updateLevel(Level $level,
+                                EntityManagerInterface $entityManager,
+                                Request $request)
+    {
+        $form = $this->createForm(LevelType::class, $level);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($level);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_index');
+        }
+        return $this->render('level/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
